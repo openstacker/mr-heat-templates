c0eb1d52e84c713498c3ef99a10249042685f476
Bartosz Górski <bartosz.gorski@ntti3.com>
2013-08-01 08:54:22 -0700

    Adding first use case template.

--- first_use_case/launch_first_use_case_nested.sh
+++ first_use_case/launch_first_use_case_nested.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-STACK_NAME='N1'
+STACK_NAME='N2'
 KEY_NAME='heat_key'
 REGION_NAME='RegionOne'
 EXTERNAL_NETWORK_ID=`neutron net-list | grep public | awk '{print $2}' | xargs -I% echo %`
@@ -10,4 +10,4 @@ SUBNET_POOL_START='10.1.0.10'
 SUBNET_POOL_END='10.1.0.200'
 VPN_PEER_CIDR='10.2.0.0/24'
 
-heat stack-create ${STACK_NAME} -f first_use_case_nested.template -P "KeyName=${KEY_NAME};RegionOne=${REGION_NAME};ExternalNetworkId=${EXTERNAL_NETWORK_ID};ExternalGatewayIPAddress=${EXTERNAL_GATEWAY_IP_ADDRESS};SubnetCidr=${SUBNET_CIDR};SubnetPoolStart=${SUBNET_POOL_START};SubnetPoolEnd=${SUBNET_POOL_END};VPNPeerCidr=${VPN_PEER_CIDR}"
+heat stack-create ${STACK_NAME} -f first_use_case_nested.template -P "KeyName=${KEY_NAME};RegionName=${REGION_NAME};ExternalNetworkId=${EXTERNAL_NETWORK_ID};ExternalGatewayIPAddress=${EXTERNAL_GATEWAY_IP_ADDRESS};SubnetCidr=${SUBNET_CIDR};SubnetPoolStart=${SUBNET_POOL_START};SubnetPoolEnd=${SUBNET_POOL_END};VPNPeerCidr=${VPN_PEER_CIDR}"
