f21dcdb7369cc1047d337959de4ef971c219065c
Bartosz Górski <bartosz.gorski@ntti3.com>
2013-12-01 04:34:49 -0800

    Reorganazing common an first use case templates

--- common/launch_network_setup.sh
+++ common/launch_network_setup.sh
@@ -4,10 +4,8 @@ TIME=`date +"%H%M%S"`
 STACK_NAME="VPN_${TIME}"
 
 EXTERNAL_NETWORK_NAME_OR_ID='public'
-EXTERNAL_GATEWAY_IP_ADDRESS='172.24.4.233'
 SUBNET_CIDR='10.1.0.0/24'
 SUBNET_POOL_START='10.1.0.10'
 SUBNET_POOL_END='10.1.0.200'
-VPN_PEER_CIDR='10.2.0.0/24'
 
-heat stack-create ${STACK_NAME} -f network_setup.template -P "ExternalNetworkNameOrId=${EXTERNAL_NETWORK_NAME_OR_ID};ExternalGatewayIPAddress=${EXTERNAL_GATEWAY_IP_ADDRESS};SubnetCidr=${SUBNET_CIDR};SubnetPoolStart=${SUBNET_POOL_START};SubnetPoolEnd=${SUBNET_POOL_END};VPNPeerCidr=${VPN_PEER_CIDR}"
+heat stack-create ${STACK_NAME} -f network_setup.template -P "ExternalNetworkNameOrId=${EXTERNAL_NETWORK_NAME_OR_ID};SubnetCidr=${SUBNET_CIDR};SubnetPoolStart=${SUBNET_POOL_START};SubnetPoolEnd=${SUBNET_POOL_END}"
--- first_use_case/launch_first_use_case_nested.sh
+++ first_use_case/launch_first_use_case_nested.sh
@@ -3,11 +3,7 @@
 STACK_NAME='N1'
 KEY_NAME='heat_key'
 REGION_NAME='RegionOne'
-EXTERNAL_NETWORK_NAME_OR_ID='public'
-SUBNET_CIDR='10.1.0.0/24'
-SUBNET_POOL_START='10.1.0.10'
-SUBNET_POOL_END='10.1.0.200'
-VPN_PEER_CIDR='10.2.0.0/24'
+SUBNET_ID=`neutron subnet-list | grep private | awk '{print $2}'`
 
-heat stack-create ${STACK_NAME} -f first_use_case_nested.template -P "KeyName=${KEY_NAME};RegionName=${REGION_NAME};ExternalNetworkNameOrId=${EXTERNAL_NETWORK_NAME_OR_ID};SubnetCidr=${SUBNET_CIDR};SubnetPoolStart=${SUBNET_POOL_START};SubnetPoolEnd=${SUBNET_POOL_END};VPNPeerCidr=${VPN_PEER_CIDR}"
 
+heat stack-create ${STACK_NAME} -f first_use_case_nested.template -P "KeyName=${KEY_NAME};RegionName=${REGION_NAME};SubnetId=${SUBNET_ID}"
